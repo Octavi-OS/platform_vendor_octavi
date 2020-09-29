8206ca60c410b6af5761a081a71c321347d52887
Sagarrokade006 <sagarrokade006@gmail.com>
2020-09-29 12:25:40 +0000

    Rearrange Versioning
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -1,9 +1,39 @@
-GET_VERSION := vendor/octavi/build/tools/getversion.py
-OCTAVI_STATUS := $(shell $(GET_VERSION) status)
-BUILD_TYPE := $(shell $(GET_VERSION) buildtype)
-OCTAVI_BRANDING_VERSION := $(BUILD_TYPE)
-OCTAVI_VERSION := $(shell $(GET_VERSION) version)
+OCTAVI_STATUS = 1.0
+
+ifndef OCTAVI_BUILD_TYPE
+    OCTAVI_BUILD_TYPE := Unofficial
+endif
 
 ifneq ($(SIGNING_KEYS),)
     PRODUCT_DEFAULT_DEV_CERTIFICATE := $(SIGNING_KEYS)/releasekey
 endif
+
+CURRENT_DEVICE := $(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
+LIST := $(shell cat vendor/octavi/octavi.devices)
+
+ifeq ($(filter $(CURRENT_DEVICE), $(LIST)), $(CURRENT_DEVICE))
+    ifeq ($(filter-out Officla OFFICIAL, $(OCTAVI_BUILD_TYPE)),)
+        ifeq ($(OCTAVI_BUILD_TYPE), Official)
+          BUILD_TYPE := Official
+        endif
+        ifeq ($(OCTAVI_BUILD_TYPE), OFFICIAL)
+          BUILD_TYPE := OFFICIAL
+        endif
+    endif
+else
+    ifeq ($(filter-out Official OFFICIAL, $(OCTAVI_BUILD_TYPE)),)
+      $(error "Invalid OCTAVI_BUILD_TYPE!")
+    endif
+endif
+
+TARGET_PRODUCT_SHORT := $(subst octavi_,,$(TARGET_PRODUCT))
+
+OCTAVI_VERSION := OctaviOS-v$(OCTAVI_STATUS)-$(TARGET_PRODUCT_SHORT)-$(shell date -u +%Y%m%d)-$(OCTAVI_BUILD_TYPE)
+
+OCTAVI_BRANDING_VERSION := $(OCTAVI_BUILD_TYPE)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.build.project=octavi \
+  ro.octavi.version=$(OCTAVI_VERSION) \
+  ro.octavi.status=$(OCTAVI_STATUS) \
+  ro.octavi.branding.version=$(OCTAVI_BRANDING_VERSION)
