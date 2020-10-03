decf5fedbdccc8f03a0272f06fa2fda68c236d86
jhenrique09 <jhenrique09@users.noreply.github.com>
2020-10-03 21:14:47 +0000

    vendor: Allow providing camera server and service
    Change-Id: I6d56930f4673cf7bdf99fccb87c15f0c0fc9a1d7

--- config/BoardConfigOctavi.mk
+++ config/BoardConfigOctavi.mk
@@ -1,2 +1,8 @@
 include vendor/octavi/config/BoardConfigKernel.mk
 include vendor/octavi/config/BoardConfigSoong.mk
+
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/av/camera/cameraserver \
+    frameworks/av/services/camera/libcameraservice
+endif
