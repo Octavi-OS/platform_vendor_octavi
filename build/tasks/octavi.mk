5e830ffb0a3d6de22e9fc43f1449e0604a936f84
Sukeerat Goindi <sukeeratgoindi@gmail.com>
2020-11-11 20:31:56 +0530

    Add colored ASCII Art

--- build/tasks/octavi.mk
+++ build/tasks/octavi.mk
@@ -16,9 +16,29 @@
 # -----------------------------------------------------------------
 # Octavi OTA update package
 
+# Build system colors
+ifneq ($(BUILD_WITH_COLORS),0)
+  CL_RED="\033[31m"
+  CL_GRN="\033[32m"
+  CL_YLW="\033[33m"
+  CL_BLU="\033[34m"
+  CL_MAG="\033[35m"
+  CL_CYN="\033[36m"
+  CL_RST="\033[0m"
+endif
+
 OCTAVI_TARGET_PACKAGE := $(PRODUCT_OUT)/$(OCTAVI_VERSION).zip
 
 .PHONY: octavi
 octavi: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(OCTAVI_TARGET_PACKAGE)
 	@echo "Package Complete: $(OCTAVI_TARGET_PACKAGE)" >&2
+	echo -e ${CL_GRN}"    ____  _______________ _    ______   ____  _____       "${CL_RST}
+	echo -e ${CL_RED}"   / __ \/ ____/_  __/   | |  / /  _/  / __ \/ ___/       "${CL_RST}
+	echo -e ${CL_CYN}"  / / / / /     / / / /| | | / // /   / / / /\__ \        "${CL_RST} 
+	echo -e ${CL_BLU}" / /_/ / /___  / / / ___ | |/ // /   / /_/ /___/ /        "${CL_RST}
+	echo -e ${CL_MAG}" \____/\____/ /_/ /_/  |_|___/___/   \____//____/         "${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"================================================="${CL_RST}
+	echo -e ${CL_BLD}${CL_YLW}"Zip: "${CL_YLW} $(OCTAVI_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_YLW}"Size:"${CL_YLW}" `du -sh $(OCTAVI_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_CYN}"====================================================="${CL_RST}
