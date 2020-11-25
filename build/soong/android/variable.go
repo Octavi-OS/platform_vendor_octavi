5ea2da2bf3682e06b0335d8ffb9b204324dd4df1
jhenrique09 <jhenrique09@users.noreply.github.com>
2020-11-25 08:10:59 +0000

    vendor: Add soong variable for TARGET_CAMERA_BOOTTIME_TIMESTAMP

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -68,6 +68,10 @@ type Product_variables struct {
 		Cppflags []string
 		Shared_libs []string
 	}
+        Needs_camera_boottime_timestamp struct {
+		Cflags []string
+		Srcs []string
+	}
 }
 
 type ProductVariables struct {
@@ -94,4 +98,5 @@ type ProductVariables struct {
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
+        Needs_camera_boottime_timestamp  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -25,6 +25,7 @@ $(call add_json_bool, Target_uses_prebuilt_dynamic_partitions,   $(filter true,$
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
 $(call add_json_bool, Target_ignores_ftp_pptp_conntrack_failure, $(filter true,$(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
+$(call add_json_bool, Needs_camera_boottime_timestamp, $(filter true,$(TARGET_CAMERA_BOOTTIME_TIMESTAMP)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
