94a4b8192f5414ee29f6ce7941184c40e15117bb
neobuddy89 <neobuddy89@users.noreply.github.com>
2021-12-11 01:30:11 +0000

    Add our favourite fonts in AOSP FontEngine [2/2] Signed-off-by: Pranav Vashi <neobuddy89@gmail.com> Signed-off-by: blinoff82 <blinov.in@gmail.com> Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com> Change-Id: I49072bec4403e341db5edc0d0ccc38c59b6fb517

--- config/common.mk
+++ config/common.mk
@@ -68,6 +68,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Vendor_045e_Product_0719.kl
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/octavi/prebuilt/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log
