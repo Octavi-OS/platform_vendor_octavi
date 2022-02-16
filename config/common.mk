Project-Awaken/android_vendor_awaken

vendor: Add game overlay sysconfig
Change-Id: Ie5b46b86125f1fe601bce93a24256607c7ea28d7
Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

config/common.mk
@@ -94,6 +94,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Fonts
 include vendor/awaken/config/fonts.mk
 
+# Game Overlay
+PRODUCT_COPY_FILES += \
+    vendor/awaken/prebuilt/common/etc/sysconfig/game_overlay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_overlay.xml
+
 # Gapps
 ifeq ($(USE_GAPPS),true)
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)

Octavi-OS/platform_vendor_octavi

config/common.mk
@@ -80,6 +80,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
+# Game Overlay
+PRODUCT_COPY_FILES += \
+    vendor/octavi/prebuilt/common/etc/sysconfig/game_overlay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_overlay.xml
+
 # Gapps
 ifeq ($(WITH_GAPPS),true)
 $(call inherit-product, vendor/gms/products/gms.mk)